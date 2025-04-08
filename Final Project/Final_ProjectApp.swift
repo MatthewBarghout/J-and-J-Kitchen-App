16c1777a2012221937ed1669a4e11c4d50900795
MatthewBarghout <mattbarg@unc.edu>
2025-04-08 09:47:54 -0400

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Matthew Barghout on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
